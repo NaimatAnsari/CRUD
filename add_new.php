56dd924749172a6fb6864fb5b6470b26746befe4
Naimat Ansari <123116024+NaimatAnsari@users.noreply.github.com>
2024-12-12 23:08:08 +0500

    add

---
+++ add_new.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD Application</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+
+</head>
+<body>
+    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
+    PHP CRUD Application
+    </nav>
+
+    <div class="container">
+        <div class="text-center mb-4">
+            <h3>Add New User</h3>
+            <p class="text-muted">Complete the form below to add a new user</p>
+        </div>
+
+        <div class="container d-flex justify-content-center">
+            <form action="" method="post" style="width:50vw; min-width:300px;">
+                <div class="row">
+                    <div class="col">
+                        <label class="form-label">First Name:</label>
+                        <input type="text" class="form-control" name="first_name" placeholder="Enter your first name" required>
+                    </div>
+
+                    <div class="col">
+                        <label class="form-label">Last Name:</label>
+                        <input type="text" class="form-control" name="last_name" placeholder="Enter your last name"
+                        required>
+                    </div>
+
+                    <div class="mb-3 mt-3">
+                    <label class="form-label">Email:</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter your email"
+                    required>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label >Gender:</label>&nbsp;
+                        <input type="radio" class="form-check-input" name="gender" id="male" value="male" required>
+                        <label for="male" class="form-input-label">Male</label>
+                        &nbsp;
+                        <input type="radio" class="form-check-input" name="gender" id="female" value="female" required>
+                        <label for="female" class="form-input-label">Female</label>
+                    </div>
+
+                    <div>
+                        <button type="submit" class="btn btn-success" name="submit">Save</button>
+                        <a href="" class="btn btn-danger">Cancel</a>
+                    </div>  
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS (Optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- db-conn.php
+++ db-conn.php
@@ -3,14 +3,13 @@
 $server = "localhost";
 $user ="root";
 $password = "";
-$database = "crud_2e1";
+$database = "y_crud";
 
 $conn = mysqli_connect($server,$user,$password,$database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 
 
 ?>
